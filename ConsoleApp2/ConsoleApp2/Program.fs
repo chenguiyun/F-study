9256b3e117865126d686088f482a8d91511a91a6
cgy <cgy@ng.yn.cn>
2019-04-19 16:37:31 +0800

    浮点

--- ConsoleApp2/ConsoleApp2/Program.fs
+++ ConsoleApp2/ConsoleApp2/Program.fs
@@ -9,7 +9,13 @@ let test() =
    for i = 1 to 9 do
       for j = 1 to 9 do
          printfn "%d * %d = %d"i j (re i j)
+let a=50000000.0
+let b=99999999999.00000001
+let c=a * b;
+let ca()=printfn "%f" c
+
 [<EntryPoint>]
 let main argv =
-    test()
+    //test()
+    ca()
     0 // return an integer exit code
